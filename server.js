cc35a5ab26940e7f4cf774c0b3ed993867865425
gkravatsky <gkravats@gmail.com>
2023-09-11 22:37:08 -0400

    test

--- server.js
+++ server.js
@@ -1,40 +1,36 @@
-var http = require('http'), 
-    fs = require('fs'), 
-    port = 8080;
+var http = require('http'),
+  fs = require('fs'),
+  url = require('url'), 
+  port = 8080;
 
 /* Global variables */
 var listingData, server;
 
-var requestHandler = function(request, response) {
-  /*Investigate the request object. 
+var requestHandler = function (req, res) { // Correct function signature
+  /* Investigate the request object.
     You will need to use several of its properties: url and method
   */
-  //console.log(request);
 
-  /*
-    Your request handler should send listingData in the JSON format as a response if a GET request 
-    is sent to the '/listings' path. Otherwise, it should send a 404 error. 
+  // Parse the URL of the incoming request
+  var parsedUrl = url.parse(req.url, true);
 
-    HINT: Explore the request object and its properties 
-    HINT: Explore the response object and its properties
-    https://code.tutsplus.com/tutorials/http-the-protocol-every-web-developer-must-know-part-1--net-31177
-    
-    HINT: Explore how callback's work 
-    http://www.theprojectspot.com/tutorial-post/nodejs-for-beginners-callbacks/4
-    
-    HINT: Explore the list of MIME Types
-    https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
-   
-    HINT: Explore mdn web docs for resources on how to use javascript.
-    Helpful example: if-else structure- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else
+  if (parsedUrl.pathname === '/listings' && req.method === 'GET') { 
+    // Set the response headers to indicate JSON content
+    res.writeHead(200, { 'Content-Type': 'application/json' });
 
-    */
+    // Convert the listingData object to JSON and send it as the response
+    res.end(JSON.stringify(listingData));
+  } else {
+    // Handle other requests
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Bad gateway error');
+  }
 };
 
-fs.readFile('listings.json', 'utf8', function(err, data) {
+fs.readFile('listings.json', 'utf8', function (err, data) {
   /*
-    This callback function should save the data in the listingData variable, 
-    then start the server. 
+    This callback function should save the data in the listingData variable,
+    then start the server.
 
     HINT: Check out this resource on fs.readFile
     //https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
@@ -43,18 +39,20 @@ fs.readFile('listings.json', 'utf8', function(err, data) {
     http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
    */
 
-    //Check for errors
-    /*this resource gives you an idea of the general format err objects and Throwing an existing object.
-    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/throw#throwing_an_existing_object
-   */
-  
+  // Check for errors
+  if (err) {
+    console.error('Error reading file:', err);
+    return;
+  }
 
-   //Save the data in the listingData variable already defined
-  
+  // Save the data in the listingData variable already defined
+  listingData = JSON.parse(data);
 
-  //Creates the server
-  
-  //Start the server
+  // Creates the server
+  server = http.createServer(requestHandler); // Pass the requestHandler function here
 
-
+  // Start the server
+  server.listen(port, () => {
+    console.log(`Server is listening on http://localhost:${port}`);
+  });
 });
